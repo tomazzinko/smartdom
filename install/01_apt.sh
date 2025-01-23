684994a10bc04f7916b4b0be902bf5fdef7a62cf
Tomaz Zinko <tomaz@zinko.si>
2025-01-23 11:03:01 +0100

    feat: hello ansible

--- install/01_apt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-apt -y update
-
-apt -y install docker docker-compose zsh python3-pip python3-venv mosquitto
--- install/99_docker_up.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-sudo su - ${SMARTDOM_USER} << EOF
-    cd $SMARTDOM_HOME
-    docker-compose up -d
-EOF
